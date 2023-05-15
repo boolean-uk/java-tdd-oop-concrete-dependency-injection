c6af3474d4602c94a41355f7fff1e52e766a61f9
Panagiotis Tziolos <PanagiotisTziolos@outlook.com>
2023-05-15 11:33:11 +0300

    tests passed

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -3,21 +3,30 @@ package com.booleanuk.core;
 import java.util.ArrayList;
 
 public class Computer {
+    public PowerSupply psu;
     public ArrayList<Game> installedGames = new ArrayList<>();
 
+    public Computer(PowerSupply psu) {
+        this.psu = psu;
+    }
+
+    public Computer(PowerSupply psu, ArrayList<Game> games) {
+        this.psu = psu;
+        this.installedGames = games;
+    }
+
     public void turnOn() {
-        PowerSupply psu = new PowerSupply();
         psu.turnOn();
     }
 
-    public void installGame() {
-        Game game = new Game("Morrowind");
+    public void installGame(String gameTitle) {
+        Game game = new Game(gameTitle);
         this.installedGames.add(game);
     }
 
-    public String playGame() {
+    public String playGame(String gameTitle) {
         for (Game g : this.installedGames) {
-            if (g.name.equals("Morrowind")) {
+            if (g.name.equals(gameTitle)) {
                 return g.start();
             }
         }
