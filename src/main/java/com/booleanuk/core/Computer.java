e323ba3abf22c4bff29524bae00654c6bc57f1fa
Rose <rosevandergiessen@proton.me>
2023-05-15 10:50:20 +0200

    tests pass

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -1,23 +1,33 @@
 package com.booleanuk.core;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Computer {
     public ArrayList<Game> installedGames = new ArrayList<>();
+    public PowerSupply psu;
 
-    public void turnOn() {
-        PowerSupply psu = new PowerSupply();
-        psu.turnOn();
+    public Computer(PowerSupply psu){
+        this.psu = psu;
     }
 
-    public void installGame() {
-        Game game = new Game("Morrowind");
+    public Computer(PowerSupply psu, ArrayList<Game> preInstalled){
+        this.psu = psu;
+        this.installedGames = preInstalled;
+    }
+
+    public void turnOn() {
+        this.psu.turnOn();
+    }
+
+    public void installGame(String gameName) {
+        Game game = new Game(gameName);
         this.installedGames.add(game);
     }
 
-    public String playGame() {
+    public String playGame(String gameName) {
         for (Game g : this.installedGames) {
-            if (g.name.equals("Morrowind")) {
+            if (g.name.equals(gameName)) {
                 return g.start();
             }
         }
