795c647d455dc75f645258fe243b0fc0a018ff4f
Carmen Barcia <carmen.barcia@hotmail.com>
2023-05-15 10:41:02 +0200

    "Test passed"

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -4,20 +4,32 @@ import java.util.ArrayList;
 
 public class Computer {
     public ArrayList<Game> installedGames = new ArrayList<>();
+    PowerSupply psu;
+
+    public Computer(PowerSupply psu){
+        this.psu = psu;
+
+    }
+
+    public Computer(PowerSupply psu, ArrayList<Game> preInstalledGames){
+        this.psu = psu;
+        this.installedGames = preInstalledGames;
+    }
 
     public void turnOn() {
-        PowerSupply psu = new PowerSupply();
         psu.turnOn();
     }
 
-    public void installGame() {
-        Game game = new Game("Morrowind");
+
+
+    public void installGame(String name) {
+        Game game = new Game(name);
         this.installedGames.add(game);
     }
 
-    public String playGame() {
+    public String playGame(String name) {
         for (Game g : this.installedGames) {
-            if (g.name.equals("Morrowind")) {
+            if (g.name.equals(name)) {
                 return g.start();
             }
         }
