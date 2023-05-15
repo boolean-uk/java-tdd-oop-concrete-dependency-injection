15e04f037a8cefb1d692ace6b38e58d05a82c670
ThanosPa <papanikolaoua7@gmail.com>
2023-05-15 12:01:44 +0300

    core

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -5,19 +5,29 @@ import java.util.ArrayList;
 public class Computer {
     public ArrayList<Game> installedGames = new ArrayList<>();
 
+    public PowerSupply psu;
+
+    public Computer(PowerSupply psu) {
+        this.psu = psu;
+    }
+    public Computer(PowerSupply psu,ArrayList<Game> preinstalled) {
+        this.psu=psu;
+        this.installedGames = preinstalled;
+    }
+
     public void turnOn() {
-        PowerSupply psu = new PowerSupply();
+        //PowerSupply psu = new PowerSupply();
         psu.turnOn();
     }
 
-    public void installGame() {
-        Game game = new Game("Morrowind");
+    public void installGame(String stringGame) {
+        Game game = new Game(stringGame);
         this.installedGames.add(game);
     }
 
-    public String playGame() {
+    public String playGame(String stringGame) {
         for (Game g : this.installedGames) {
-            if (g.name.equals("Morrowind")) {
+            if (g.name.equals(stringGame)) {
                 return g.start();
             }
         }
