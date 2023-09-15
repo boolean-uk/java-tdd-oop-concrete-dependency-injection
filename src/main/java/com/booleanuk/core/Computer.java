350b2d158e43e3efbed018142453db60171b9057
iliask796 <iliaskal7@gmail.com>
2023-09-15 11:33:44 +0300

    Changes to finish Core

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -3,25 +3,38 @@ package com.booleanuk.core;
 import java.util.ArrayList;
 
 public class Computer {
-    public ArrayList<Game> installedGames = new ArrayList<>();
+    private final PowerSupply psu;
+    private final ArrayList<Game> installedGames;
 
-    public void turnOn() {
-        PowerSupply psu = new PowerSupply();
-        psu.turnOn();
+    public Computer(PowerSupply psu){
+        this.psu = psu;
+        this.installedGames = new ArrayList<>();
     }
 
-    public void installGame() {
-        Game game = new Game("Morrowind");
+    public Computer(PowerSupply psu, ArrayList<Game> installedGames){
+        this.psu = psu;
+        this.installedGames = installedGames;
+    }
+
+    public ArrayList<Game> getInstalledGames() {
+        return installedGames;
+    }
+
+    public void turnOn() {
+        this.psu.turnOn();
+    }
+
+    public void installGame(String gameName) {
+        Game game = new Game(gameName);
         this.installedGames.add(game);
     }
 
-    public String playGame() {
+    public String playGame(String gameName) {
         for (Game g : this.installedGames) {
-            if (g.name.equals("Morrowind")) {
+            if (g.getName().equals(gameName)) {
                 return g.start();
             }
         }
-
         return "Game not installed";
     }
 }
