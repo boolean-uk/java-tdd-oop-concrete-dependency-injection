c9eadd4e41dbb72acfd345481a833dd206ab7d4a
Petros Moustakis <petrosmoukas16@gmail.com>
2023-09-15 11:34:08 +0300

    exercise done

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -4,20 +4,31 @@ import java.util.ArrayList;
 
 public class Computer {
     public ArrayList<Game> installedGames = new ArrayList<>();
+    private PowerSupply psu;
+
+    public Computer() {}
+
+    public Computer(PowerSupply psu) {
+        this.psu = psu;
+    }
+
+    public Computer( PowerSupply psu, ArrayList<Game> installedGames) {
+        this.psu = psu;
+        this.installedGames = installedGames;
+    }
 
     public void turnOn() {
-        PowerSupply psu = new PowerSupply();
         psu.turnOn();
     }
 
-    public void installGame() {
-        Game game = new Game("Morrowind");
+    public void installGame(String newGame) {
+        Game game = new Game(newGame);
         this.installedGames.add(game);
     }
 
-    public String playGame() {
+    public String playGame(String game) {
         for (Game g : this.installedGames) {
-            if (g.name.equals("Morrowind")) {
+            if (g.name.equals(game)) {
                 return g.start();
             }
         }
@@ -25,3 +36,5 @@ public class Computer {
         return "Game not installed";
     }
 }
+
+
