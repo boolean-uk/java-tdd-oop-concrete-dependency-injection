37ce717c8a59f01d89cfae5e7e0ac1b793aaad39
Hiko Yüksek <33344671+hkyksk@users.noreply.github.com>
2024-01-19 10:40:15 +0100

    Refactored game class for injection; passed all tests

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -3,25 +3,33 @@ package com.booleanuk.core;
 import java.util.ArrayList;
 
 public class Computer {
+    private final PowerSupply powerSupply;
     public ArrayList<Game> installedGames = new ArrayList<>();
 
+    public Computer(PowerSupply psu1) {
+        this.powerSupply = psu1;
+    }
+
+    public Computer(PowerSupply psu1, ArrayList<Game> preInstalledGames) {
+        this.powerSupply = psu1;
+        this.installedGames.addAll(preInstalledGames);
+    }
+
     public void turnOn() {
-        PowerSupply psu = new PowerSupply();
-        psu.turnOn();
+        powerSupply.turnOn();
     }
 
-    public void installGame() {
-        Game game = new Game("Morrowind");
-        this.installedGames.add(game);
+    public void installGame(String gameName) {
+        Game game = new Game(gameName);
+        installedGames.add(game);
     }
 
-    public String playGame() {
-        for (Game g : this.installedGames) {
-            if (g.name.equals("Morrowind")) {
+    public String playGame(String gameName) {
+        for (Game g : installedGames) {
+            if (g.name.equals(gameName)) {
                 return g.start();
             }
         }
-
         return "Game not installed";
     }
 }
