b45e73c531bbbd4b5e0e8539c4928b46c8613557
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-19 09:00:49 +0100

    Fixed tests

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -4,20 +4,28 @@ import java.util.ArrayList;
 
 public class Computer {
     public ArrayList<Game> installedGames = new ArrayList<>();
+    private PowerSupply psu;
 
+    public Computer(PowerSupply myPsu) {
+        this.psu = myPsu;
+    }
+
+    public Computer(PowerSupply myPsu, ArrayList<Game> preInstalled) {
+        this.installedGames.addAll(preInstalled);
+        this.psu = myPsu;
+    }
     public void turnOn() {
-        PowerSupply psu = new PowerSupply();
         psu.turnOn();
     }
 
-    public void installGame() {
-        Game game = new Game("Morrowind");
+    public void installGame(String gameTitle) {
+        Game game = new Game(gameTitle);
         this.installedGames.add(game);
     }
 
-    public String playGame() {
+    public String playGame(String gameTitle) {
         for (Game g : this.installedGames) {
-            if (g.name.equals("Morrowind")) {
+            if (g.name.equals(gameTitle)) {
                 return g.start();
             }
         }
