348474c7be2ae3bf3ddaf81d672a8c7c00370d81
Nicolai Klokmose <angryniczorn@gmail.com>
2024-01-19 09:21:08 +0100

    Nicolai Klokmose & Marit

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -4,24 +4,37 @@ import java.util.ArrayList;
 
 public class Computer {
     public ArrayList<Game> installedGames = new ArrayList<>();
+    private PowerSupply psu;
+
+    public Computer(PowerSupply psu) {
+        this.setPsu(psu);
+    }
+
+    public Computer (PowerSupply psu, ArrayList<Game> preInstalled) {
+        this.setPsu(psu);
+        this.installedGames.addAll(preInstalled);
+    }
 
     public void turnOn() {
-        PowerSupply psu = new PowerSupply();
-        psu.turnOn();
+        //PowerSupply psu = new PowerSupply();
+        this.psu.turnOn();
     }
 
-    public void installGame() {
-        Game game = new Game("Morrowind");
-        this.installedGames.add(game);
+    public void installGame(String gameName) {
+        //Game game = new Game(gameName);
+        this.installedGames.add(new Game(gameName));
     }
 
-    public String playGame() {
+    public String playGame(String gameName) {
         for (Game g : this.installedGames) {
-            if (g.name.equals("Morrowind")) {
+            if (g.name.equals(gameName)) {
                 return g.start();
             }
         }
-
         return "Game not installed";
+    }
+
+    public void setPsu(PowerSupply psu) {
+        this.psu = psu;
     }
 }
