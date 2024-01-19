2566a8116d24eeb618d0e4be7defa10a72a01e15
Robin Kaastrup <robin.kaastrup@no.experis.com>
2024-01-19 09:28:38 +0100

    Refactored methods in Computer.java

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -19,14 +19,13 @@ public class Computer {
         this.powerSupply.turnOn();
     }
 
-    public void installGame(String gameName) {
-        Game game = new Game(gameName);
+    public void installGame(Game game) {
         this.installedGames.add(game);
     }
 
-    public String playGame(String gameName) {
+    public String playGame(Game game) {
         for (Game g : this.installedGames) {
-            if (g.name.equals(gameName)) {
+            if (g.equals(game)) {
                 return g.start();
             }
         }
