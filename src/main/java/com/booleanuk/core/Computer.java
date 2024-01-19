e26256a0891388c55748ffefd9b0565f0edc3fce
Robin Kaastrup <robin.kaastrup@no.experis.com>
2024-01-19 09:16:58 +0100

    All test are now passing

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -4,20 +4,29 @@ import java.util.ArrayList;
 
 public class Computer {
     public ArrayList<Game> installedGames = new ArrayList<>();
+    private PowerSupply powerSupply;
 
-    public void turnOn() {
-        PowerSupply psu = new PowerSupply();
-        psu.turnOn();
+    public Computer(PowerSupply powerSupply) {
+        this.powerSupply = powerSupply;
     }
 
-    public void installGame() {
-        Game game = new Game("Morrowind");
+    public Computer(PowerSupply powerSupply, ArrayList<Game> preInstalled) {
+        this.powerSupply = powerSupply;
+        installedGames.addAll(preInstalled);
+    }
+
+    public void turnOn() {
+        this.powerSupply.turnOn();
+    }
+
+    public void installGame(String gameName) {
+        Game game = new Game(gameName);
         this.installedGames.add(game);
     }
 
-    public String playGame() {
+    public String playGame(String gameName) {
         for (Game g : this.installedGames) {
-            if (g.name.equals("Morrowind")) {
+            if (g.name.equals(gameName)) {
                 return g.start();
             }
         }
