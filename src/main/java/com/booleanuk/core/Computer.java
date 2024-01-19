512c88c79caf3d2384bc4dc31b403ab07bdd95e2
Petter Karstensen <petterkarstensen@gmail.com>
2024-01-19 10:25:00 +0100

    Extension

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -4,26 +4,32 @@ import java.util.ArrayList;
 
 public class Computer {
     public ArrayList<Game> installedGames = new ArrayList<>();
-    public PowerSupply powerSupply;
+    private PowerSupply powerSupply;
+    private Game game;
 
-    public Computer(PowerSupply powerSupply){
-        this.powerSupply = powerSupply;
+    public Computer(){
+        this.powerSupply = new PowerSupply();
     }
+
     public Computer(PowerSupply powerSupply, ArrayList<Game> installedGames){
         this.powerSupply = powerSupply;
         this.installedGames = installedGames;
+
+    }
+    public PowerSupply getPowerSupply(){
+        return powerSupply;
     }
     public void turnOn() {
         powerSupply.turnOn();
     }
     public void installGame(String newGame) {
-        Game game = new Game(newGame);
+        this.game = new Game(newGame);
         this.installedGames.add(game);
     }
 
     public String playGame(String playGame) {
         for (Game g : this.installedGames) {
-            if (g.name.equals(playGame)) {
+            if (g.getName().equals(playGame)) {
                 return g.start();
             }
         }
