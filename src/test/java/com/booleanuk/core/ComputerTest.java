9152a22b8b06337f0bbd0d1ceb0c4d9eb7047de0
Dag Andreas <daanfofo@gmail.com>
2024-08-20 09:34:44 +0200

    rebased extention branch into main

--- src/test/java/com/booleanuk/core/ComputerTest.java
+++ src/test/java/com/booleanuk/core/ComputerTest.java
@@ -53,4 +53,5 @@ class ComputerTest {
         Assertions.assertEquals("Dwarf Fortress", myPc.installedGames.get(0).name);
         Assertions.assertEquals("Baldur's Gate", myPc.installedGames.get(1).name);
     }
+
 }
