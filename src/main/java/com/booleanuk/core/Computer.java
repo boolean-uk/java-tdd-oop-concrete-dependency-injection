925953fdbf3274fe30e27649d3975c2f17935ac9
Jonas Finborud Nyman <jonas.nyman@no.experis.com>
2025-01-13 09:49:34 +0100

    completed exercise

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -4,20 +4,34 @@ import java.util.ArrayList;
 
 public class Computer {
     public ArrayList<Game> installedGames = new ArrayList<>();
+    public PowerSupply psu;
+
+    public Computer(PowerSupply psu) {
+        this.psu = psu;
+    }
+
+    public Computer(PowerSupply psu, ArrayList<Game> preInstalled) {
+        this.psu = psu;
+        installedGames.addAll(preInstalled);
+    }
 
     public void turnOn() {
-        PowerSupply psu = new PowerSupply();
         psu.turnOn();
     }
 
-    public void installGame() {
-        Game game = new Game("Morrowind");
-        this.installedGames.add(game);
+    public void installGame(String game) {
+        for(Game g : installedGames) {
+            if(g.name.equals(game)) {
+                System.out.println("Game is already installed.");
+                return;
+            }
+        }
+        this.installedGames.add(new Game(game));
     }
 
-    public String playGame() {
+    public String playGame(String game) {
         for (Game g : this.installedGames) {
-            if (g.name.equals("Morrowind")) {
+            if (g.name.equals(game)) {
                 return g.start();
             }
         }
